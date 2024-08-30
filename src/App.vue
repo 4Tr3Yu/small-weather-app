f47ddc3aa4203dc036b015d910b36ee2368621e2
Jorge Francisco Aguirre Concha <jorgef.aguir@gmail.com>
2024-08-30 16:30:44 -0400

    footer was bothering me

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@
 </script>
 
 <template>
-	<div class="container mx-auto relative min-h-dvh ">
+	<div class="container mx-auto flex flex-col relative min-h-dvh ">
 		<Header/>
-		<main class="px-2">
+		<main class="px-2 mb-auto">
 			<Tabs/>
 			<HourlyWeatherDisplay city="Rio de Janeiro" />
 			<DailyWeatherDisplay city="Rio de Janeiro" />
